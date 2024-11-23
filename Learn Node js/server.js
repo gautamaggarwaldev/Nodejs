77b50f647964e81604edc8c31a9791d29dbe3d1f
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-11-23 11:19:00 +0530

    basename delimeter

--- Learn Node js/server.js
+++ Learn Node js/server.js
@@ -1,5 +1,13 @@
-var http = require("http");
-http.createServer(function (req, res) {
-    res.write("GG ki Pathshala!!!!");
-    res.end();
-}).listen(8080);
+// var http = require("http");
+// http.createServer(function (req, res) {
+//     res.write("GG ki Pathshala!!!!");
+//     res.end();
+// }).listen(8080);
+
+
+const { log } = require("console");
+var path = require("path");
+// console.log(path.basename(__dirname));
+// console.log(__dirname);
+
+console.log(path.delimiter);
