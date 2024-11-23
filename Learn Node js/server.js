1f8b9d8079bb8b4dd60d247d9bd37a3e6b16d3f1
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-11-23 11:24:18 +0530

    basename vs dirname

--- Learn Node js/server.js
+++ Learn Node js/server.js
@@ -10,4 +10,12 @@ var path = require("path");
 // console.log(path.basename(__dirname));
 // console.log(__dirname);
 
-console.log(path.delimiter);
+// console.log(path.delimiter);
+console.log(path.dirname); //[Function: dirname]
+console.log(path.dirname("users/gg/new folder/index.js")); //users/gg/new folder
+console.log(path.basename("users/gg/new folder/index.js")); // index.js
+
+
+
+console.log(path.join("/users", "/new folder", "/index.html"))
+console.log(path.join(__dirname, "/index.html"))
